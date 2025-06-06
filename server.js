ce7bb9e45114bd17cf67b3c67119b74d4608f558
Kedar Vyas <kedar.vyas@heliossolutions.co>
2025-06-06 18:12:38 +0530

    multiple streams working

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// app.use(express.static('public'));
+
+// io.on('connection', (socket) => {
+//     console.log("Client connected:", socket.id);
+
+//     socket.on("offer", (offer) => {
+//         socket.broadcast.emit("offer", offer);
+//     });
+
+//     socket.on("answer", (answer) => {
+//         socket.broadcast.emit("answer", answer);
+//     });
+
+//     socket.on("ice-candidate", (candidate) => {
+//         socket.broadcast.emit("ice-candidate", candidate);
+//     });
+// });
+
+// server.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -8,52 +38,41 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let streamerSocketId = null;
-let viewerSocketId = null;
-
 io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
 
-    socket.on("role", (role) => {
-        if (role === "streamer") {
-            streamerSocketId = socket.id;
-            console.log("Streamer connected:", streamerSocketId);
-        } else if (role === "viewer") {
-            viewerSocketId = socket.id;
-            console.log("Viewer connected:", viewerSocketId);
-        }
-    });
-
+    // Streamer sends an offer
     socket.on("offer", (offer) => {
-        if (viewerSocketId) {
-            io.to(viewerSocketId).emit("offer", offer);
-        }
+        console.log("Received offer from", socket.id);
+        // Send the offer to all other clients (e.g., admin) with streamerId attached
+        socket.broadcast.emit("offer", {
+            offer,
+            streamerId: socket.id,
+        });
     });
 
-    socket.on("answer", (answer) => {
-        if (streamerSocketId) {
-            io.to(streamerSocketId).emit("answer", answer);
-        }
+    // Admin sends answer back to specific streamer
+    socket.on("answer", ({ answer, streamerId }) => {
+        console.log("Sending answer to", streamerId);
+        io.to(streamerId).emit("answer", answer);
     });
 
-    socket.on("ice-candidate", (candidate) => {
-        // Forward ICE candidates to the *other* peer
-        if (socket.id === streamerSocketId && viewerSocketId) {
-            io.to(viewerSocketId).emit("ice-candidate", candidate);
-        } else if (socket.id === viewerSocketId && streamerSocketId) {
-            io.to(streamerSocketId).emit("ice-candidate", candidate);
+    // ICE candidate from streamer or admin
+    socket.on("ice-candidate", ({ candidate, streamerId }) => {
+        if (streamerId) {
+            // From admin to streamer
+            io.to(streamerId).emit("ice-candidate", candidate);
+        } else {
+            // From streamer to admin(s)
+            socket.broadcast.emit("ice-candidate", {
+                candidate,
+                streamerId: socket.id,
+            });
         }
     });
 
     socket.on("disconnect", () => {
-        if (socket.id === streamerSocketId) {
-            streamerSocketId = null;
-            console.log("Streamer disconnected");
-        }
-        if (socket.id === viewerSocketId) {
-            viewerSocketId = null;
-            console.log("Viewer disconnected");
-        }
+        console.log("Client disconnected:", socket.id);
     });
 });
 
